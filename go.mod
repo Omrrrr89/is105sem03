c480759c3808129d08f2dc0a5ea74aa5b285211c
omrrr omrrr <omrrr@MacBook-Pro-81.local>
2023-04-20 23:22:12 +0200

    Ahmad1

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Omrrrr89/is105sem03
+
+go 1.19
--- main.go
+++ main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"fmt"
-	"github.com/uia-worker/is105sem03/mycrypt"
+	"github.com/Omrrrr89/is105sem03/mycrypt"
 )
 
 func main() {
